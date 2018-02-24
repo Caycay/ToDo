d21709085a617c71182cc0fe7e15cdc0e50300be
Karolina <karolina.sanetra@capsilon.com>
2018-02-24 12:33:35 +0100

    delete

--- reactapp/lib/const.js
+++
@@ -1,9 +0,0 @@
-export const apiServer = {
-    url: 'http://localhost:62848',
-    method: {
-        lists: '/api/list',
-        listWithId: '/api/list/:id',
-        items: '/api/item',
-        itemWithId: '/api/item/:id'
-    }
-};
